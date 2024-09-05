45599367856ab92fcdf118a2708765e8b604f139
sitaram kudireddy <sitaram.kudireddy@xcubelabs.com>
2024-09-05 14:53:28 +0530

    prettier issue fixed

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(3200);
 }
 bootstrap();
